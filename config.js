950c136ebf95148f38dc7f3e6544d6bc86d36079
duong1998nd <94595403+duong1998nd@users.noreply.github.com>
2022-11-21 11:37:54 +0700

    Add files via upload
    chúc ngủ ngon

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://facebook.com/duong1009nd';
+const eventName = 'Click vào đây nha';
+const titleCard = 'Chúc em ngủ ngon';
+const contentCard = 'Chúc em ngủ ngon nha, chúc e mơ thật nhiều giấc mơ đẹp....e cứ ấn vào ra nhiều màu đẹp lắm ó 💘';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
